4b6ba95e74ca875e2c009817280413e50c248430
KiberDron <andreynotebook@gmail.com>
2023-04-22 03:07:54 +0300

    added cors origin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://devicestore.netlify.app",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use(fileUpload({}));
